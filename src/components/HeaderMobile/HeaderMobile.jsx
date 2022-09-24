a030c4bcd46cabc736fc9c05b1842e482cc4cc1a
byKriz <cristianesquivelh@hotmail.com>
2022-09-24 03:10:40 -0400

    boton de display menu

--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import styles from "./HeaderMobile.module.scss";
 import logoSVG from "../../assets/header/ninja-base-mobile-logo.svg";
 import ninjaLogoSVG from "../../assets/header/logo.svg";
-import hamburgerMenu from "../../assets/header/hamburger.svg";
+import hamburgerMenuOpen from "../../assets/header/hamburger.svg";
+import hamburgerMenuClose from "../../assets/header/hamburger-close.svg";
 
 export const HeaderMobile = () => {
   const [menu, setMenu] = useState(false);
@@ -12,15 +13,34 @@ export const HeaderMobile = () => {
   };
 
   return (
-    <header className={styles.header}>
+    <header className={styles.headerMobile}>
       <div className={styles.headerTopContainer}>
         <a href="/">
-            <img src={logoSVG} alt="logo" className={styles.logo} />
+          <img src={logoSVG} alt="logo" className={styles.logo} />
         </a>
         <a href="/">
-            <img src={ninjaLogoSVG} alt="ninja logo" className={styles.ninjaLogo}/>
+          <img
+            src={ninjaLogoSVG}
+            alt="ninja logo"
+            className={styles.ninjaLogo}
+          />
         </a>
-        <img src={hamburgerMenu} alt="hamburger-menu" className={styles.hamMenu} />
+
+        {!menu ? (
+          <img
+            src={hamburgerMenuOpen}
+            alt="hamburger-menu"
+            className={styles.hamMenu}
+            onClick={handleMenu}
+          />
+        ) : (
+          <img
+            src={hamburgerMenuClose}
+            alt="hamburger-menu-close"
+            className={styles.hamMenu}
+            onClick={handleMenu}
+          />
+        )}
       </div>
       <div></div>
     </header>
