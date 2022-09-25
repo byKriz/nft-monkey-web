a23046c7d8704e5067962fb16dccfeb7dd3426fd
byKriz <cristianesquivelh@hotmail.com>
2022-09-24 21:49:24 -0400

    integrando la la page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { HeaderMobile } from "./components/HeaderMobile/HeaderMobile";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <HeaderMobile />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
