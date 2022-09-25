e9413db343b5a3b2775cbd6a06c471314fc06cbb
byKriz <cristianesquivelh@hotmail.com>
2022-09-24 21:56:16 -0400

    aplicando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { HeaderMobile } from "./components/HeaderMobile/HeaderMobile";
-import { Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <HeaderMobile />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <HeaderMobile />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
