b17ccfc3d53272756499b6c03872dff933ad1ec6
byKriz <cristianesquivelh@hotmail.com>
2022-09-26 01:07:31 -0400

    incorporando heroCONTAINER EN LA PAGE HOME

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { HeroContainer } from "../../components/HeroContainer/HeroContainer";
+import styles from "./Home.module.scss"
 
 export const Home = () => {
   return (
-    <>
-    
-    </>
+    <div className={styles.homeContainer}>
+        <HeroContainer />
+    </div>
   );
 };
