baca7e622a94070d700b443d8bae3e1f1820f4e9
byKriz <cristianesquivelh@hotmail.com>
2022-09-29 14:51:07 -0400

    agregando cards al slide

--- src/components/SlideContainerRoadmap/SlideContainerRoadmap.jsx
+++ src/components/SlideContainerRoadmap/SlideContainerRoadmap.jsx
@@ -4,6 +4,7 @@ import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { RoadmapMobileCard } from "../RoadmapMobileCard/RoadmapMobileCard";
 import road1IMG from "@road/roadmap-1.png";
+import road2IMG from "@road/roadmap-2.png";
 
 export const SlideContainerRoadmap = () => {
   return (
@@ -14,10 +15,22 @@ export const SlideContainerRoadmap = () => {
       centeredSlides={true}
     >
       <SwiperSlide>
-        <RoadmapMobileCard img={road1IMG} />
+        <RoadmapMobileCard
+          index="01"
+          title="Sold Out!"
+          year="2021"
+          img={road1IMG}
+          desciption="Our genesis 8,888 PFP collection launched and sold out within minutes."
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <RoadmapMobileCard />
+        <RoadmapMobileCard
+          index="02"
+          title="Token Launch"
+          year="2021 [Q1]"
+          img={road2IMG}
+          desciption="Our utility token $NST is launched and every ninja NFT begins yielding $NST daily by staking."
+        />
       </SwiperSlide>
     </Swiper>
   );
