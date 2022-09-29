0d8b8c1adb565d931e662f604dc67c56128da534
byKriz <cristianesquivelh@hotmail.com>
2022-09-29 14:30:59 -0400

    agregando configuraciones del slide

--- src/components/SlideContainerRoadmap/SlideContainerRoadmap.jsx
+++ src/components/SlideContainerRoadmap/SlideContainerRoadmap.jsx
@@ -3,12 +3,18 @@ import styles from "./SlideContainerRoadmap.module.scss";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { RoadmapMobileCard } from "../RoadmapMobileCard/RoadmapMobileCard";
+import road1IMG from "@road/roadmap-1.png";
 
 export const SlideContainerRoadmap = () => {
   return (
-    <Swiper className={styles.slideContainer}>
+    <Swiper
+      className={styles.slideContainer}
+      slidesPerView={2}
+      spaceBetween={80}
+      centeredSlides={true}
+    >
       <SwiperSlide>
-        <RoadmapMobileCard />
+        <RoadmapMobileCard img={road1IMG} />
       </SwiperSlide>
       <SwiperSlide>
         <RoadmapMobileCard />
