8fd2007f2ee1b959f02e62e1b3954fcdd4c2fb1e
byKriz <cristianesquivelh@hotmail.com>
2022-09-29 14:50:52 -0400

    agregando props

--- src/components/RoadmapMobileCard/RoadmapMobileCard.jsx
+++ src/components/RoadmapMobileCard/RoadmapMobileCard.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import styles from "./RoadmapMobileCard.module.scss";
 
-export const RoadmapMobileCard = ({ img }) => {
+export const RoadmapMobileCard = ({ index, title, year, img, desciption }) => {
   const altname = (img) => `${img}`.split("/")[4];
 
   return (
     <div className={styles.RoadmapMobileCard}>
       <div className={styles.roadmapTopContainer}>
-        <span className={styles.roadmapIndex}>01</span>
+        <span className={styles.roadmapIndex}>{index}</span>
         <div className={styles.roadmapTitleYearContainer}>
-          <span className={styles.roadmapTitle}>Sold out!</span>
-          <span className={styles.roadmapYear}>2021</span>
+          <span className={styles.roadmapTitle}>{title}</span>
+          <span className={styles.roadmapYear}>{year}</span>
         </div>
       </div>
 
       <div className={styles.roadmapBottomContainer}>
         <img src={img} alt={altname(img)} />
-        <span className={styles.roadmapDescription}>
-          Our genesis 8,888 PFP collection launched and sold out within minutes.
-        </span>
+        <span className={styles.roadmapDescription}>{desciption}</span>
       </div>
     </div>
   );
