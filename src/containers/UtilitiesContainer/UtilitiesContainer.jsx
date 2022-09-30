b86f578852d60e7bf9f267260a14420041a9fd92
byKriz <cristianesquivelh@hotmail.com>
2022-09-30 16:15:26 -0400

    arreglando estilos para utility container

--- src/containers/UtilitiesContainer/UtilitiesContainer.jsx
+++ src/containers/UtilitiesContainer/UtilitiesContainer.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import styles from "./UtilitiesContainer.module.scss"
+import styles from "./UtilitiesContainer.module.scss";
 import { ButtonContainer } from "../../components/ButtonContainer/ButtonContainer";
-import borderPng from "@assets/utilityContainer/utility-border-up.png"
-import utilityTitle from "@assets/utilityContainer/utility.png"
+import borderPng from "@assets/utilityContainer/utility-border-up.png";
+import utilityTitle from "@assets/utilityContainer/utility.png";
 
 export const UtilitiesContainer = () => {
   return (
     <section className={styles.UtilitiesContainer}>
-      <img src={borderPng} alt="border-utility" className={styles.boderUtility}/>
-      <h2>A ninja has many skills:</h2>
-      <img src={utilityTitle} alt="utility-title" className={styles.utilityTitle}/>
-      <p className={styles.containerDescription}>
-        Ninja Squad NFT gives you access to the dojo and unlocks members-only
-        benefits. Join the best NFT & crypto traders to play, earn, learn and
-        trade together.
-      </p>
-      <ButtonContainer text="JOIN NOW" page="/" />
+      <img
+        src={borderPng}
+        alt="border-utility"
+        className={styles.boderUtility}
+      />
+      <div className={styles.utilityBody}>
+        <h2>A ninja has many skills:</h2>
+        <img
+          src={utilityTitle}
+          alt="utility-title"
+          className={styles.utilityTitle}
+        />
+        <p className={styles.containerDescription}>
+          Ninja Squad NFT gives you access to the dojo and unlocks members-only
+          benefits. Join the best NFT & crypto traders to play, earn, learn and
+          trade together.
+        </p>
+        <ButtonContainer text="JOIN NOW" page="/" />
+      </div>
     </section>
   );
 };
