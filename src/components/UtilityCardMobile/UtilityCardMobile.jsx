b4356863ba1376b6313e119f2e99d50f5cc9c0f4
byKriz <cristianesquivelh@hotmail.com>
2022-10-01 15:10:19 -0400

    agregando props

--- src/components/UtilityCardMobile/UtilityCardMobile.jsx
+++ src/components/UtilityCardMobile/UtilityCardMobile.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import styles from "./UtilityCardMobile.module.scss";
-import img1 from "@utility/utility-1.png";
+// import img1 from "@utility/utility-1.png";
 
-export const UtilityCardMobile = () => {
+export const UtilityCardMobile = ({ UtilImg, title, description }) => {
   const altname = (img) => `${img}`.split("/")[4];
 
   return (
     <div className={styles.UtilityCardMobile}>
-      <img src={img1} alt={altname(img1)} />
-      <span className={styles.utilityMobileTitle}>Access alpha channels</span>
-      <span className={styles.utilityMobileDescription}>
-        Make connections and - of course - money, alongside a global network of
-        NFT & crypto traders and explorers. Be part of 24/7 active channels
-        where members share their research & alpha, discuss projects, give and
-        receive feedback. Become smarter and more confident as you navigate the
-        markets.
-      </span>
+      <img src={UtilImg} alt={altname(UtilImg)} />
+      <span className={styles.utilityMobileTitle}>{title}</span>
+      <span className={styles.utilityMobileDescription}>{description}</span>
     </div>
   );
 };
