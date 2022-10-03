eaf74343f07028b1795c487f35afcd50c1b6acae
byKriz <cristianesquivelh@hotmail.com>
2022-10-03 14:30:00 -0400

    agregando un color secundario

--- src/components/ButtonContainer/ButtonContainer.jsx
+++ src/components/ButtonContainer/ButtonContainer.jsx
@@ -3,9 +3,9 @@ import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styles from "./ButtonContainer.module.scss"
 
-export const ButtonContainer = ({text, page}) => {
+export const ButtonContainer = ({text, page, secondColor=false}) => {
   return (
-    <Link className={styles.buttomContainer} to={page}>
+    <Link className={secondColor ? `${styles.buttomContainer} ${styles.secondColor}` : `${styles.buttomContainer} `} to={page}>
       <span>{text}</span>
       <FaArrowRight />
     </Link>
